b5cc8e1994b28bf6918aba899063c546e70cc071
Roy Vegard Ovesen <roy.vegard.ovesen@gmail.com>
2021-12-01 19:42:05 +0100

    Format output

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 mod day_01;
 
 fn main() {
-    println!("Day 1");
-    println!("\tPart 1: {}", day_01::part_1(String::from("input/day_01")));
-    println!("\tPart 2: {}", day_01::part_2(String::from("input/day_01")));
+    println!("Day 1: Sonar Sweep");
+    println!(
+        "\tPart One: {}",
+        day_01::part_1(String::from("input/day_01"))
+    );
+    println!(
+        "\tPart Two: {}",
+        day_01::part_2(String::from("input/day_01"))
+    );
 }
