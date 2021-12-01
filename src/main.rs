3a94ee0c412d52e0d05661bf6f4d110dd7ca18db
Roy Vegard Ovesen <roy.vegard.ovesen@gmail.com>
2021-12-01 18:34:14 +0100

    Format output

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 mod day_01;
 
 fn main() {
-    println!(
-        "Day 1\n\tPart 1: {}",
-        day_01::part_1(String::from("input/day_01"))
-    );
-
-    println!(
-        "Day 1\n\tPart 2: {}",
-        day_01::part_2(String::from("input/day_01"))
-    );
+    println!("Day 1");
+    println!("\tPart 1: {}", day_01::part_1(String::from("input/day_01")));
+    println!("\tPart 2: {}", day_01::part_2(String::from("input/day_01")));
 }
